36c5076a323ce6054bcbe720a94503e6f532a67a
andreyalek <andreyalek@gmail.com>
2011-10-18 17:29:24 +0300

    Implemented. We can delete only unused newsletters

--- admin/helpers/newsletter.php
+++ admin/helpers/newsletter.php
@@ -165,7 +165,7 @@ class NewsletterHelper
 		// Get info about newsletter plus info about using it in lists.
 		$dbo = JFactory::getDbo();
 		$query = $dbo->getQuery(true);
-		$query->select('DISTINCT ns.*, (CASE WHEN l.list_id IS NULL THEN 0 ELSE 1 END) AS used_as_static')
+		$query->select("DISTINCT ns.*, (CASE WHEN l.list_id IS NULL THEN '0' ELSE '1' END) AS used_as_static")
 			->from('#__newsletter_newsletters AS ns')
 			->join('LEFT', '#__newsletter_lists AS l ON (ns.newsletter_id = l.send_at_reg OR ns.newsletter_id = l.send_at_unsubscribe)')
 			->where('newsletter_id=' . (int) $id);
@@ -175,7 +175,7 @@ class NewsletterHelper
 
 		if (!empty($data)) {
 			// Check if we can change the type of newsletter
-			$data['type_changeable'] = (!$data['used_as_static'] && $data['sent_started'] == '0000-00-00 00:00:00');
+			$data['type_changeable'] = ($data['used_as_static'] == 0 && $data['sent_started'] == '0000-00-00 00:00:00');
 			$data['saveable'] = ($data['type'] == 1 || $data['sent_started'] == '0000-00-00 00:00:00');
 			return $data;
 		}
