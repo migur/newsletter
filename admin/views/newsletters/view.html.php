8431a3f83af078085304a83722797f996e1e30bd
andreyalek <andrey@migur.com>
2011-11-01 17:43:21 +0200

    0008418: Bug. Incorrect time calculating caused by time transitions. Resolved

--- admin/views/newsletters/view.html.php
+++ admin/views/newsletters/view.html.php
@@ -136,11 +136,10 @@ class NewsletterViewNewsletters extends MigurView
 		);
 		JavascriptHelper::addObject('statActiveSubscribersCount', $res);
 
-		$theDay = 3600 * 24;
 		$now = date('Y-m-d H:i:s');
-		$sevenDaysBefore = date('Y-m-d', time() - $theDay * 7) . " 00:00:00";
-		$thirtyDaysBefore = date('Y-m-d', time() - $theDay * 30) . " 00:00:00";
-		$ninetyDaysBefore = date('Y-m-d', time() - $theDay * 90) . " 00:00:00";
+		$sevenDaysBefore = date('Y-m-d', strtotime('-7 Days', time())) . " 00:00:00";
+		$thirtyDaysBefore = date('Y-m-d', strtotime('-30 Days', time())) . " 00:00:00";
+		$ninetyDaysBefore = date('Y-m-d', strtotime('-90 Days', time())) . " 00:00:00";
 
 
 		$this->totalSubs = array(
