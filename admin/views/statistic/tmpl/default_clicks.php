fde304a1d04907831af4e3729e6296c67e78d5b6
andreyalek <andrey@migur.com>
2011-12-20 16:27:47 +0200

    Added new chart. Removed TOTAL SENT  and  TOTAL CLICKS pie charts

--- admin/views/statistic/tmpl/default_clicks.php
+++ admin/views/statistic/tmpl/default_clicks.php
@@ -1,16 +1,12 @@
 <div id="stat-clicks">
 	<dl>
-		<dt><b>Number of total clicks</b></dt>
-		<dd>
-			<div id="stat-total-clicks"></div>
-		</dd>
+		<dt><b>Sents, Clicks, Bounces, Views</b></dt>
+		<dd><div id="all-in-one"></div></dd>
+		
 		<dt>
-			<b>Number of clicks and views per day</b></dt>
-		<dd>
-			<div id="days-selector">
+			<b>Number of clicks and views per day</b>
+			<div class="fltrt">
 				<div id="days-label"><?php echo JText::_('COM_NEWSLETTER_SELECT_RANGE'); ?></div>
-
-
 				<select name="days">
 					<?php echo JHtml::_('select.options', array(
 						'5'  => '5 '   . JText::_('COM_NEWSLETTER_DAYS'),
@@ -21,10 +17,10 @@
 						'60' => '60 '  . JText::_('COM_NEWSLETTER_DAYS'),
 					), 'value', 'text', array($this->days)); ?>
 				</select>
-			</div>
-			<div id="stat-clicks-per-day"></div>
-
-		</dd>
+			</div>	
+		</dt>
+		<dd><div id="stat-clicks-per-day"></div></dd>
+		
 		<dt><b>Number of clicks and views per hour (last 24 hours)</b></dt>
 		<dd>
 			<div id="stat-clicks-per-hour"></div>
@@ -36,14 +32,35 @@
 </div>
 
 <script type="text/javascript">
-
-	legend = { 
-		data:[ ["## Total clicks"] ],	
-		position: 'south'
-	}
-	data = [ statTotalClicks.total ];
-	var ccPie = Migur.chart.pie("stat-total-clicks", legend, data, 70, 70, 60);
-
+	
+	data.labels = [
+		'Total sent', 
+		'Total clicks', 
+		'Bounced mails', 
+		'Active subscribers',
+		'Opened mails']
+	
+	data.views = [
+		[
+			statTotalSent.total,
+			statTotalClicks.total,
+			statTotalSent.hard + statTotalSent.soft + statTotalSent.technical,
+			statActiveCount.subscribers,
+			statActiveCount.newsletters
+		]
+	];
+	
+	var pdRaph = Migur.chart.hline.draw("all-in-one", data.labels, data.views, [], {
+		chart: {
+			width: 500,
+			height: 110,
+			padding: 10,
+			graph: {
+				// Wery important that this dimensions matches with 
+				// NON-FRACTIONAL step of bars. Try several values to get the best result.
+				width:  430,
+				height: 100	}}
+	});
 
 
 	data = { views:[], clicks:[], labels:[]	};
@@ -52,7 +69,7 @@
 		data.labels.push(key);
 		data.views.push(viewsPerDay[key]);
 	});
-	var pdRaph = Migur.chart.line("stat-clicks-per-day", data.labels, [ data.views, data.clicks ], 578, 150, []);
+	var pdRaph = Migur.chart.line("stat-clicks-per-day", data.labels, [ data.views, data.clicks ], 578, 100, []);
 
 
 
@@ -62,6 +79,6 @@
 		dataHour.labels.push(key.substr(11,2));
 		dataHour.views.push(viewsPerHour[key]);
 	});
-	var phRaph = Migur.chart.line("stat-clicks-per-hour", dataHour.labels, [ dataHour.views, dataHour.clicks ], 578, 150, []);
+	var phRaph = Migur.chart.line("stat-clicks-per-hour", dataHour.labels, [ dataHour.views, dataHour.clicks ], 578, 100, []);
 
 </script>
