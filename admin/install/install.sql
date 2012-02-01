c7ecd3ac4e23bb7e0ab0e23b3c2e93c1fd38b8c7
andreyalek <andrey@migur.com>
2012-02-01 17:14:23 +0200

    Added tables for logs

--- admin/install/install.sql
+++ admin/install/install.sql
@@ -281,6 +281,39 @@ CREATE TABLE `#__newsletter_automailing_targets` (
 ) ENGINE=INNODB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
 
 
+
+CREATE TABLE `#__newsletter_logs` (
+  `log_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(255),
+  `content` TEXT,
+  `data` BLOB,
+  `created_on` DATETIME NOT NULL,
+  `created_by` INT(11),
+  `category` VARCHAR(255),
+  `subject_table` VARCHAR(11),
+  `subject_id` INT(11),
+
+  PRIMARY KEY (`log_id`)
+) ENGINE=INNODB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+
+CREATE INDEX `created_on_idfk` ON `#__newsletter_logs`(`created_on`);
+CREATE INDEX `subject_tableid_idfk` ON `#__newsletter_logs`(`subject_table`, `subject_id`);
+
+
+
+CREATE TABLE `#__newsletter_log_users` (
+  `log_user_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `log_id` INT(11),
+  `user_id` INT(11),
+  `action` ENUM('viewed'),
+
+  PRIMARY KEY (`log_user_id`)
+) ENGINE=INNODB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+
+CREATE INDEX `user_id_idxfk` ON `#__newsletter_log_users`(`user_id`, `action`);
+
+
+
 CREATE INDEX `smtp_profile_id_idxfk` ON `#__newsletter_newsletters`(`smtp_profile_id`);
 # ALTER TABLE `#__newsletter_newsletters` ADD CONSTRAINT `newsletters_smtp_profile_idfk` FOREIGN KEY `smtp_profile_id_idxfk` (`smtp_profile_id`) REFERENCES `#__newsletter_smtp_profiles` (`smtp_profile_id`);
 
