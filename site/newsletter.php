6dd9c25678900cf28663e75b273b701fa15b4878
andreyalek <andrey@migur.com>
2012-08-15 11:57:24 +0300

    Wrapped main files into try-catch updated helpers' names

--- site/newsletter.php
+++ site/newsletter.php
@@ -23,57 +23,76 @@ if (!defined('COM_NEWSLETTER_PATH_SITE')) {
 	define('COM_NEWSLETTER_PATH_SITE', JPATH_COMPONENT_SITE);
 }	
 
-// import joomla controller library
-jimport('joomla.application.component.controller');
-jimport('joomla.application.component.view');
-jimport('joomla.form.helper');
-jimport('migur.migur');
-jimport('joomla.error.log');
+try {
 
-// Add the helper
-JLoader::import('helpers.plugin', JPATH_COMPONENT_ADMINISTRATOR, '');
-JLoader::import('helpers.javascript', JPATH_COMPONENT_ADMINISTRATOR, '');
-JLoader::import('helpers.rssfeed', JPATH_COMPONENT_ADMINISTRATOR, '');
-JLoader::import('helpers.log', JPATH_COMPONENT_ADMINISTRATOR, '');
-JHtml::addIncludePath(JPATH_COMPONENT_ADMINISTRATOR. DIRECTORY_SEPARATOR .'helpers'. DIRECTORY_SEPARATOR .'html');
+	// Run autoloader
+	JLoader::import('helpers.autoload', COM_NEWSLETTER_PATH_ADMIN, '');
+	NewsletterHelperAutoload::setup();
 
-JFormHelper::addRulePath(JPATH_COMPONENT_ADMINISTRATOR . DIRECTORY_SEPARATOR . 'models' . DIRECTORY_SEPARATOR . 'rules');
-JTable::addIncludePath(JPATH_COMPONENT_ADMINISTRATOR . DIRECTORY_SEPARATOR . 'tables');
-JModel::addIncludePath(JPATH_COMPONENT_ADMINISTRATOR . DIRECTORY_SEPARATOR . 'models');
-JModel::addIncludePath(JPATH_COMPONENT_ADMINISTRATOR . DIRECTORY_SEPARATOR . 'models', 'NewsletterModel');
-JModel::addIncludePath(JPATH_COMPONENT_ADMINISTRATOR . DIRECTORY_SEPARATOR . 'models' . DIRECTORY_SEPARATOR . 'entities', 'NewsletterModelEntity');
 
-// Get an instance of the controller prefixed by Newsletter
-$controller = JController::getInstance('Newsletter');
+	// import joomla controller library
+	jimport('joomla.application.component.controller');
+	jimport('joomla.application.component.view');
+	jimport('joomla.form.helper');
+	jimport('migur.migur');
+	jimport('joomla.error.log');
 
-// ACL
-	$resource = JRequest::getString('view','') .'.'. JRequest::getString('layout','default');
+	// Add the helper
+	JLoader::import('helpers.plugin', JPATH_COMPONENT_ADMINISTRATOR, '');
+	JLoader::import('helpers.javascript', JPATH_COMPONENT_ADMINISTRATOR, '');
+	JLoader::import('helpers.rssfeed', JPATH_COMPONENT_ADMINISTRATOR, '');
+	JLoader::import('helpers.log', JPATH_COMPONENT_ADMINISTRATOR, '');
+	JHtml::addIncludePath(JPATH_COMPONENT_ADMINISTRATOR. DIRECTORY_SEPARATOR .'helpers'. DIRECTORY_SEPARATOR .'html');
+
+	JFormHelper::addRulePath(JPATH_COMPONENT_ADMINISTRATOR . DIRECTORY_SEPARATOR . 'models' . DIRECTORY_SEPARATOR . 'rules');
+	JTable::addIncludePath(JPATH_COMPONENT_ADMINISTRATOR . DIRECTORY_SEPARATOR . 'tables');
+	JModel::addIncludePath(JPATH_COMPONENT_ADMINISTRATOR . DIRECTORY_SEPARATOR . 'models');
+	JModel::addIncludePath(JPATH_COMPONENT_ADMINISTRATOR . DIRECTORY_SEPARATOR . 'models', 'NewsletterModel');
+	JModel::addIncludePath(JPATH_COMPONENT_ADMINISTRATOR . DIRECTORY_SEPARATOR . 'models' . DIRECTORY_SEPARATOR . 'entities', 'NewsletterModelEntity');
+
+	// Get an instance of the controller prefixed by Newsletter
+	$controller = JController::getInstance('Newsletter');
+
+	// ACL
+		$resource = JRequest::getString('view','') .'.'. JRequest::getString('layout','default');
+
+		switch($resource){
+
+			case 'subscribe.unsubscribe':
+
+				if(!JFactory::getUser()->id && !JRequest::getString('uid', NULL)) {
+
+					JFactory::getApplication()->redirect(
+						JRoute::_('index.php?option=com_users&view=login&returnurl=' . base64_encode(JRoute::_('index.php?option=com_newsletter&view=subscribe&layout=unsubscribe', false))), 
+						JText::_('COM_NEWSLETTER_LOGIN_FIRST'), 
+						'message');
+				}	
+		}
+
+	// Add translations used in JavaScript
+	JavascriptHelper::requireTranslations();
+
+	MigurPluginHelper::prepare();
+
+	$app = JFactory::getApplication();
+	$app->triggerEvent('onMigurStart');
+
+	// Perform the Request task
+	$controller->execute(JRequest::getCmd('task'));
+
+	$app->triggerEvent('onMigurShoutdown');
+
+	// Redirect if set by the controller
+	$controller->redirect();
+
+} catch (Exception $e) {
 	
-	switch($resource){
-		
-		case 'subscribe.unsubscribe':
-			
-			if(!JFactory::getUser()->id && !JRequest::getString('uid', NULL)) {
-				
-				JFactory::getApplication()->redirect(
-					JRoute::_('index.php?option=com_users&view=login&returnurl=' . base64_encode(JRoute::_('index.php?option=com_newsletter&view=subscribe&layout=unsubscribe', false))), 
-					JText::_('COM_NEWSLETTER_LOGIN_FIRST'), 
-					'message');
-			}	
-	}
+	NewsletterHelperLog::addDebug(
+		'COM_NEWSLETTER_UNCAUGHT_EXCEPTION',
+		'common',
+		$e);
+	
+	throw $e;
+}
 
-// Add translations used in JavaScript
-JavascriptHelper::requireTranslations();
-
-MigurPluginHelper::prepare();
-
-$app = JFactory::getApplication();
-$app->triggerEvent('onMigurStart');
-
-// Perform the Request task
-$controller->execute(JRequest::getCmd('task'));
-
-$app->triggerEvent('onMigurShoutdown');
-
-// Redirect if set by the controller
-$controller->redirect();
+	
