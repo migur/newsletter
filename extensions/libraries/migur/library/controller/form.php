c700b851b4313d0a6299375ff1c7ff3016392f9d
andreyalek <andrey@migur.com>
2012-09-13 17:05:28 +0300

    Fix controller classes and help toolbar button

---
+++ extensions/libraries/migur/library/controller/form.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * The extended version of JModelList.
+ *
+ * @version	   $Id:  $
+ * @copyright  Copyright (C) 2011 Migur Ltd. All rights reserved.
+ * @license	   GNU General Public License version 2 or later; see LICENSE.txt
+ */
+// No direct access
+defined('JPATH_BASE') or die;
+
+// Check if Migur is active
+if (!defined('MIGUR')) {
+	// TODO deprecated since 12.1 Use PHP Exception
+	die(JError::raiseWarning(0, JText::_("MIGUR library wasn't found.")));
+}
+
+/**
+ * Class extends the functionality of JModelList
+ *
+ * @since   1.0
+ * @package Migur.Newsletter
+ * 
+ * @deprecated since 12.05
+ */
+if (!class_exists('JControllerFormLegacy')) {
+	class JControllerFormLegacy extends JControllerForm {}
+}
+
+class MigurControllerForm extends JControllerFormLegacy
+{}
