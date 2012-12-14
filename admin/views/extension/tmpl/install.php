c43e3f2fad6093735adfba1e3f281ddf9890549f
andreyalek <andrey@migur.com>
2012-12-14 17:33:37 +0200

    Add description for Install Extensions button on dashboard.

--- admin/views/extension/tmpl/install.php
+++ admin/views/extension/tmpl/install.php
@@ -1,2 +1,5 @@
-<center>
-<h3>Coming Soon...</h3>
+<center><h2>Install extensions</h2></center>
+<h3 style="margin:10px;">
+Installation of extensions is available for premium version only. 
+Please visit <a target="_blank" href="http://migur.com/products/newsletter">migur.com</a> to get premium version.
+</h3>
