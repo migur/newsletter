18d297fc1b470770336a1352b9c1e2abbc305bc5
andreyalek <andrey@migur.com>
2013-04-19 19:33:10 +0300

    Removed iframe border

--- site/views/show/tmpl/default.php
+++ site/views/show/tmpl/default.php
@@ -1,4 +1,5 @@
-<iframe 
+<iframe
+	frameBorder="0"
 	style="border:0; min-height:600px; max-height:2000px;"
 	width="100%" 
 	src="<?php echo JRoute::_('index.php?option=com_newsletter&task=newsletter.render&type=html&alias=' . $this->newsletter['alias'], false); ?>"
