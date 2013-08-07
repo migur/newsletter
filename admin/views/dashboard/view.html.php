eced63856ad89c5dd7af2329ce891c9209d46edc
Andrey <andrey@migur.com>
2013-08-07 14:34:12 +0300

    Fixed Migur buttons class naming

--- admin/views/dashboard/view.html.php
+++ admin/views/dashboard/view.html.php
@@ -121,7 +121,7 @@ class NewsletterViewDashboard extends MigurView
 		$bar->appendButton('Separator');
 		$bar->appendButton('Migurhelp', 'help', 'COM_NEWSLETTER_HELP_ABOUT_QUEUE', NewsletterHelperSupport::getResourceUrl('com-newsletter/mailing/general'));
 		JToolBarHelper::custom('', 'progress', '', '', false);
-		$bar->appendButton('MigurQueue', 'queue');
+		$bar->appendButton('Migurqueue', 'queue');
 
 		JToolBarHelper::custom('', 'progress', '', '', false);
 
@@ -139,7 +139,7 @@ class NewsletterViewDashboard extends MigurView
 
 		$bar = MigurToolbar::getInstance('help-toolbar');
 		$bar->appendButton('Popup', 'publish', 'COM_NEWSLETTER_ABOUT', 'http://migur.com/products/newsletter', 800, 600, 0, 0);
-		$bar->appendButton('MigurHelp', 'help', 'COM_NEWSLETTER_HELP', 'http://migur.com/support/documentation/newsletter');
+		$bar->appendButton('Migurhelp', 'help', 'COM_NEWSLETTER_HELP', 'http://migur.com/support/documentation/newsletter');
 
 		// Load the submenu.
 		NewsletterHelperNewsletter::addSubmenu(JRequest::getVar('view'));
--- admin/views/install/view.html.php
+++ admin/views/install/view.html.php
@@ -59,7 +59,7 @@ class NewsletterViewInstall extends MigurView
 		$bar->appendButton('Standard', 'restore', 'COM_NEWSLETTER_RESTORE', 'install.restore', false);
 //		$bar->appendButton('Standard', 'unpublish', 'JTOOLBAR_DISABLE', 'install.unpublish', false);
 //		$bar->appendButton('Standard', 'publish', 'JTOOLBAR_ENABLE', 'install.publish', false);
-		$bar->appendButton('MigurHelp', 'help', 'COM_NEWSLETTER_HELP', NewsletterHelperSupport::getResourceUrl('com-newsletter/extension/manager'));
+		$bar->appendButton('Migurhelp', 'help', 'COM_NEWSLETTER_HELP', NewsletterHelperSupport::getResourceUrl('com-newsletter/extension/manager'));
 
 		// Load the submenu.
 		NewsletterHelperNewsletter::addSubmenu(JRequest::getVar('view'));
