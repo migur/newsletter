3b715c2782f2bbed8366ca109bc391bf80eb9d3a
Andrey <andrey@migur.com>
2013-08-07 15:10:47 +0300

    Updated upstream artefacts fixed

--- admin/views/newsletter/sidebar.js
+++ admin/views/newsletter/sidebar.js
@@ -1,41 +1,32 @@
 
-window.addEvent('domready', 
+window.addEvent('domready',
     function() {
-        var 
+        var
             sidebar = $('htmlconstructor-sidebar'),
             viewport = $(window),
-<<<<<<< HEAD
             treshold = sidebar.getPosition().y + 100;
 
-	
-=======
-            treshold = sidebar.getPosition().y - 20;
 
->>>>>>> development
         var fixSidebar = function(){
-            
+
             if (sidebar.hasClass('fixed')) return;
-            
+
             sidebar.addClass('fixed');
-//            var pos    = sidebar.getPosition(), 
+//            var pos    = sidebar.getPosition(),
 //                scroll = viewport.getScroll();
-//            
+//
 //            sidebar.setStyle('top', scroll.y - pos.y);
         }
 
         var releaseSidebar = function(){
 
             if (!sidebar.hasClass('fixed')) return;
-            
+
             sidebar.removeClass('fixed');
         }
 
         $(viewport).addEvent('scroll', function(){
 
-<<<<<<< HEAD
-        	console.log(sidebar, viewport, treshold);
-=======
->>>>>>> development
             var scroll = viewport.getScroll();
 
             if (scroll.y > treshold) {
