0d1669736ab51579899bccd1ce54f8e694b0e7eb
Andrey <andrey@migur.com>
2013-08-16 14:09:24 +0300

    Removed obsolete settings and code from module.

--- admin/helpers/subscriber.php
+++ admin/helpers/subscriber.php
@@ -196,20 +196,11 @@ class NewsletterHelperSubscriber
 
 		// Get User ID
 		$user = $facebook->getUser();
-		if ($user) {
-			try {
-				$user_profile = $facebook->api('/me');
-			} catch (FacebookApiException $e) {
-				error_log($e);
-				$user = null;
-			}
-		}
 		// We may or may not have this data based on whether the user is logged in.
 		//
 		// If we have a $user id here, it means we know the user is logged into
 		// Facebook, but we don't know if the access token is valid. An access
 		// token is invalid if the user logged out of Facebook.
-
 		if ($user) {
 			try {
 				// Proceed knowing you have a logged in user who's authenticated.
