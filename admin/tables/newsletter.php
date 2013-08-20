9f45f7fce56eec5a7fa9298a312153ec7940fc08
Andrey <andrey@migur.com>
2013-08-20 17:16:36 +0300

    Moved PUBLISH method from tables to parent MigurTable
    Conflicts:
    	admin/tables/newsletter.php

--- admin/tables/newsletter.php
+++ admin/tables/newsletter.php
@@ -35,5 +35,65 @@ class NewsletterTableNewsletter extends MigurTable
 				$_db
 		);
 	}
+
+
+	/**
+	 * Method to set the publishing state for a row or list of rows in the database
+	 * table.  The method respects checked out rows by other users and will attempt
+	 * to checkin rows that it can after adjustments are made.
+	 *
+	 * @param	mixed	An optional array of primary key values to update.  If not
+	 * 					set the instance property value is used.
+	 * @param	integer The publishing state. eg. [0 = unpublished, 1 = published, 2=archived, -2=trashed]
+	 * @param	integer The user id of the user performing the operation.
+	 *
+	 * @return	boolean	True on success.
+	 * @since	1.0
+	 */
+	public function publish($pks = null, $state = 1, $userId = 0)
+	{
+		// Initialise variables.
+		$k = $this->_tbl_key;
+
+		// Sanitize input.
+		JArrayHelper::toInteger($pks);
+		$userId = (int) $userId;
+		$state = (int) $state;
+
+		// If there are no primary keys set check to see if the instance key is set.
+		if (empty($pks)) {
+			if ($this->$k) {
+				$pks = array($this->$k);
+			}
+			// Nothing to set publishing state on, return false.
+			else {
+				$this->setError(JText::_('JLIB_DATABASE_ERROR_NO_ROWS_SELECTED'));
+				return false;
+			}
+		}
+
+		// Get an instance of the table
+		$table = JTable::getInstance('Subscriber', 'NewsletterTable');
+
+		// For all keys
+		foreach ($pks as $pk) {
+			// Load the banner
+			if (!$table->load($pk)) {
+				$this->setError($table->getError());
+			}
+
+			$table->reset();
+			// Change the state
+			$table->{$table->_tbl_key} = $pk;
+			$table->state = $state;
+
+
+			// Store the row
+			if (!$table->store()) {
+				$this->setError($table->getError());
+			}
+		}
+		return count($this->getErrors()) == 0;
+	}
 }
 
