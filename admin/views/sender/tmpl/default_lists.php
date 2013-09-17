7862f51867c1513e1567551a5edeb0687abd9d54
Andrey <andrey@migur.com>
2013-09-17 18:58:55 +0300

    Fixed "Newsletter name" to "List Name" in sender

--- admin/views/sender/tmpl/default_lists.php
+++ admin/views/sender/tmpl/default_lists.php
@@ -10,7 +10,7 @@
                                 <input type="checkbox" name="checkall-toggle" value="" onclick="checkAll(this)" />
                         </th>
                         <th width="30%" class="left">
-                                <?php echo JHtml::_('grid.sort', 'COM_NEWSLETTER_NEWSLETTER_NAME', 'a.name', $this->lists->listDirn, $this->lists->listOrder); ?>
+                                <?php echo JHtml::_('grid.sort', 'COM_NEWSLETTER_LIST_NAME', 'a.name', $this->lists->listDirn, $this->lists->listOrder); ?>
                         </th>
                     </tr>
                 </thead>
